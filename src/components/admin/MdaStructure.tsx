c910b6f76386e0e9eb8f490ad522967b82445692
admincecdf-debug <admin.cecdf@gmail.com>
2026-07-05 13:13:37 -0300

    Create MdaStructure.tsx

---
+++ src/components/admin/MdaStructure.tsx
@@ -0,0 +1,3 @@
+// Reaproveita o mesmo componente/CRUD de MdaStructureAdmin.tsx, sob o nome que o
+// Painel 360° de Comunidade (ComunidadePanelTabs.tsx) espera, escopado por churchId.
+export { MdaStructureAdmin as MdaStructure } from "./MdaStructureAdmin";
