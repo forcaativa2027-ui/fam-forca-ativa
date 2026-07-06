18f0d954c90afbecc854ee838c6c634f51801775
admincecdf-debug <admin.cecdf@gmail.com>
2026-07-06 17:55:56 -0300

    Add files via upload

--- src/components/admin/AdminPanel.tsx
+++ src/components/admin/AdminPanel.tsx
@@ -200,8 +200,6 @@ function TabContent({ activeTab }: { activeTab: TabKey }) {
     case "finance":             return <FinanceAdmin />;
     case "patrimony":           return <PatrimonyAdmin />;
     case "gpv":                 return <GpvAdmin />;
-    case "delegations":         return <DelegationsAdmin />;
-    case "audit":               return <AuditView />;
     case "export":              return <ExportAdmin />;
     default:                    return null;
   }
